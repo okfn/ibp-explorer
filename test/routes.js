81b2aa35a1d64ce3220e15e7b2bf695f7eec0f97
Damjan Velickovski <damjan91@gmail.com>
2016-08-02 01:50:39 +0200

    Initial routes test

---
+++ test/routes.js
@@ -0,0 +1,51 @@
+'use strict'
+
+import _ from 'lodash'
+import jsdom from 'jsdom'
+import jQuery from 'jquery'
+import {expect} from 'chai'
+
+const jsdomConfig = {
+  url: "http://localhost:8080/"
+  , scripts: [jQuery]
+  , features: {
+    FetchExternalResources: ['script']
+    , ProcessExternalResources: ['script']
+    , MutationEvents: '2.0'
+    , SkipExternalResources: false
+  }
+}
+
+const routes = [
+  '#home'
+  , '#map'
+  , '#timeline'
+  , '#rankings'
+  , '#profile'
+  , '#availability'
+  , '#participation'
+  , '#download'
+]
+
+describe('Test route', function () {
+  this.timeout(35000);
+  _.forEach(routes, (route) => {
+    it(`${route}`, (done) => {
+      let config = _.extend({}, jsdomConfig)
+      config.url = config.url + route
+      config.done = (err, window) => {
+        const $ = jQuery(window)
+        const children = $('#explorer').children().length
+        window.close()
+        try {
+          expect(children).to.be.above(0)
+          done()
+        }
+        catch(e) {
+          done(new Error('No content populated for route'))
+        }
+      }
+      jsdom.env(config)
+    })
+  })
+})
