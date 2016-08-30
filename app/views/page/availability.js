dc73ad112b9661efb389002705608fa8dc641d7c
Damjan Velickovski <damjan91@gmail.com>
2016-08-30 16:42:39 +0200

    Add function for resizing the iframe

--- app/views/page/availability.js
+++ app/views/page/availability.js
@@ -5,7 +5,6 @@ import template_page from '../templates/page/availability.hbs'
 import template_row from '../templates/availability_row.hbs'
 import reportGenerator from '../reportgenerator.js'
 
-
 class ProjectPage extends Backbone.View {
 
   initialize() {
@@ -18,11 +17,12 @@ class ProjectPage extends Backbone.View {
 
   renderPage(target) {
     reportGenerator.update('2015', false)
-    this.$el.html(template_page())
+    this.$el.html(template_page({ tracker_url: TRACKER_URL + '/embed' }))
     target.html(this.$el)
     $('#year-toggles button').click(this._yearToggle)
     $('button[data-year="2015"]').click()
     $('.av-region-toggler').click(this.clickregion)
+    this._resizeCrossDomainIframe('trackerIFrame', TRACKER_URL);
   }
 
   // Private methods
@@ -48,7 +48,9 @@ class ProjectPage extends Backbone.View {
     //assert(false, "couldn't find country: " + country)
   }
 
-  _repaint(dataset = reportGenerator.dataset, questionSet = reportGenerator.questionSet, region = reportGenerator.region) {
+  _repaint(dataset = reportGenerator.dataset,
+           questionSet = reportGenerator.questionSet,
+           region = reportGenerator.region) {
     let tbody = $('#availability tbody')
     tbody.empty()
     let datasetRegions, datasetAv
@@ -96,19 +98,38 @@ class ProjectPage extends Backbone.View {
           this.$el.find('#region-0.av-region-toggler').addClass('active')
         }
       } else {
-          if (this.$el.find('#region-0.av-region-toggler').hasClass('active')) {
-            this.$el.find('#region-0.av-region-toggler').removeClass('active')
-            let index = this.regionId.indexOf(0)
-            if (index >= 0) {
-              this.regionId.splice(index, 1)
-            }
+        if (this.$el.find('#region-0.av-region-toggler').hasClass('active')) {
+          this.$el.find('#region-0.av-region-toggler').removeClass('active')
+          let index = this.regionId.indexOf(0)
+          if (index >= 0) {
+            this.regionId.splice(index, 1)
           }
-          this.regionId.push(selected)
-          target.addClass('active')
         }
+        this.regionId.push(selected)
+        target.addClass('active')
       }
+    }
     this._repaint()
     return false
+  }
+
+  _resizeCrossDomainIframe(id, other_domain) {
+    const iframe = document.getElementById(id);
+    if (window.addEventListener) {
+      window.addEventListener('message', function (event) {
+        if (event.origin !== other_domain) return;
+        if (isNaN(event.data)) return;
+        const height = parseInt(event.data);
+        iframe.height = height + "px";
+      }, false);
+    } else if (window.attachEvent) {
+      window.attachEvent('onmessage', function (event) {
+        if (event.origin !== other_domain) return;
+        if (isNaN(event.data)) return;
+        const height = parseInt(event.data);
+        iframe.height = height + "px";
+      }, false);
+    }
   }
 }
 
