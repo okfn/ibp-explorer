b48fe691ad831f735ad23dcb61a245c4deb1dc0e
Damjan Velickovski <damjan91@gmail.com>
2016-11-09 16:11:08 +0100

    Use embedable template

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use(function (req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('error_embed', {
       status: err.status,
       message: err.message,
       error: err
@@ -112,7 +112,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render('error_embed', {
     status: err.status,
     message: err.message,
     error: {}
