84e67cf45eb65f0641946666613b4772b67d5be8
Damjan Velickovski <damjan91@gmail.com>
2016-11-14 15:39:47 +0100

    Modify test for /availability, remove test for #timeline, change port

--- tests/routes.js
+++ tests/routes.js
@@ -5,8 +5,9 @@ import jsdom from 'jsdom'
 import jQuery from 'jquery'
 import {expect} from 'chai'
 
+
 const jsdomConfig = {
-  url: "http://localhost:8080/"
+  url: "http://localhost:3000/"
   , scripts: [jQuery]
   , features: {
     FetchExternalResources: ['script']
@@ -19,25 +20,29 @@ const jsdomConfig = {
 const routes = [
   '#home'
   , '#map'
-  , '#timeline'
   , '#rankings'
   , '#profile'
-  // TODO fix testing when using fetch and iframe
-  //, '#availability'
+  // TODO fix test for route #timeline - jsdom can't load #timeline in 5 minutes
+  //, '#timeline'
+  , 'availability'
   , '#participation'
   , '#download'
-  , '#search'
 ]
 
-describe('Test route', function () {
-  this.timeout(35000);
+describe('routes', function () {
+  this.timeout(50000);
   _.forEach(routes, (route) => {
     it(`${route}`, (done) => {
-      let config = _.extend({}, jsdomConfig)
+      let config = _.extend({}, jsdomConfig),
+          children
       config.url = config.url + route
       config.done = (err, window) => {
         const $ = jQuery(window)
-        const children = $('#explorer').children().length
+        if (route.charAt(0) === '#') {
+          children = $('#explorer').children().length
+        } else {
+          children = $('#tracker').children().length
+        }
         window.close()
         try {
           expect(children).to.be.above(0)
