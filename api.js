1c65bcdc61dd72c087b8e86a78d5761b36f5c2f5
Damjan Velickovski <damjan91@gmail.com>
2016-12-01 13:48:24 +0100

    Add check for the search cache being downloaded

--- api.js
+++ api.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 var Indaba = require('ibp-explorer-data-client').default.Indaba;
 var moment = require('moment');
 
+var downloading_search_cache = false
+
 function api_call (endpoint, callback) {
   var cache_file = './cache/'+endpoint+'.json';
   var should_get_from_cache = false;
@@ -26,7 +28,7 @@ function api_call (endpoint, callback) {
       if (should_update_cache) {
         fs.writeFileSync(cache_file, JSON.stringify(res));
       }
-      callback(res, new Date());
+      callback(res);
     });
   }
 }
@@ -37,16 +39,15 @@ function getSearch() {
     var cache_exists = fs.existsSync(cache_file)
     if (cache_exists) {
       var stat = fs.statSync(cache_file)
-      var hours = moment().diff(new Date(stat.mtime), 'hours')
-      if (hours > 1) {
+      var days = moment().diff(new Date(stat.mtime), 'days')
+      if (days > 1 && !downloading_search_cache) {
+        downloading_search_cache = true
         Indaba.getSearchJSON().then(function (res) {
-          resolve(res)
           fs.writeFileSync(cache_file, JSON.stringify(res))
         })
-      } else {
-        var data = fs.readFileSync(cache_file)
-        resolve(JSON.parse(data))
       }
+      var data = fs.readFileSync(cache_file)
+      resolve(JSON.parse(data))
     } else {
       Indaba.getSearchJSON().then(function (res) {
         resolve(res)
